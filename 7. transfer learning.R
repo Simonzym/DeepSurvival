2cd8dcd96b473080a432d8881916d958911f7ff7
Yimo Zhang <31706175+Simonzym@users.noreply.github.com>
2020-04-13 11:24:52 -0400

    Update 7. transfer learning.R

--- 7. transfer learning.R
+++ 7. transfer learning.R
@@ -71,8 +71,6 @@ for (k in 1:nk){
   lasso.temp.te <- exp(predict(lasso.fit,  newx = ft_total_mat[te.ind,], type = "link", s = cv.fit.lasso$lambda.min))
   lasso.pred.tr = log(lasso.temp.tr)
   lasso.pred.te = log(lasso.temp.te)
-  # lasso.pred.tr <- lasso.temp.tr/(lasso.temp.tr + 1)
-  # lasso.pred.te <- lasso.temp.te/(lasso.temp.te + 1)
   
   lasso.res <- survivalROC(Stime = surv.time[te.ind],
                            status  = status[te.ind],
@@ -92,8 +90,7 @@ for (k in 1:nk){
   ridge.temp.te <- exp(predict(ridge.fit,  newx = ft_total_mat[te.ind,], type = "link", s = cv.fit.ridge$lambda.min))
   ridge.pred.tr = log(ridge.temp.tr)
   ridge.pred.te = log(ridge.temp.te)
-    # ridge.pred.tr <- ridge.temp.tr/(ridge.temp.tr + 1)
-  # ridge.pred.te <- ridge.temp.te/(ridge.temp.te + 1)
+
   
   ridge.res <- survivalROC(Stime = surv.time[te.ind],
                            status  = status[te.ind],
@@ -105,32 +102,6 @@ for (k in 1:nk){
   ridge.ERR <- predErr(surv.tr, surv.te, ridge.pred.tr, ridge.pred.te, type = "brier", times = ts, int.type = "weighted")
   bs_ridge[k] <- ridge.ERR$ierror
   
-  
-  # # elastic-net 
-  # a <- seq(0.01, 0.99, 0.02)
-  # search <- foreach(i = a, .combine = rbind) %dopar% {
-  #   cv <- cv.glmnet(ft_total_mat[-te.ind,], surv_obj[-te.ind,], family = "cox", alpha = i)
-  #   data.frame(cvm = cv$cvm[cv$lambda == cv$lambda.min], lambda.min = cv$lambda.min, alpha = i)
-  # }
-  # param.en <- search[search$cvm == min(search$cvm),]
-  # 
-  # en.fit <- glmnet(ft_total_mat[-te.ind,], surv_obj[-te.ind,], family = "cox", lambda = param.en$lambda.min, alpha = param.en$alpha, standardize = TRUE)
-  # 
-  # en.temp.tr <- exp(predict(en.fit,  newx = ft_total_mat[-te.ind,], type = "link", s = param.en$lambda.min))
-  # en.temp.te <- exp(predict(en.fit,  newx = ft_total_mat[te.ind,], type = "link", s = param.en$lambda.min))
-  # en.pred.tr <- en.temp.tr/(en.temp.tr + 1)
-  # en.pred.te <- en.temp.te/(en.temp.te + 1)
-  # 
-  # en.res <- survivalROC(Stime = surv.time[te.ind],
-  #                       status  = status[te.ind],
-  #                       marker       = en.pred.te,
-  #                       predict.time = cutoff,
-  #                       span = 0.25*n.te^(-0.20))
-  # auc_en[k] <- en.res$AUC
-  # 
-  # en.ERR <- predErr(surv.tr, surv.te, en.pred.tr, en.pred.te, type = "brier", times = ts, int.type = "weighted")
-  # bs_en[k] <- mean(en.ERR$error, na.rm = T)
-  
   # elastic-net 2
   cv.fit.en2 <- cv.glmnet(ft_total_mat[-te.ind,], surv_obj[-te.ind,], family = "cox", alpha = 0.5)
   en.fit2 <- glmnet(ft_total_mat[-te.ind,], surv_obj[-te.ind,], family = "cox", lambda = cv.fit.en2$lambda.min, alpha = 0.5, standardize = TRUE)
@@ -139,8 +110,7 @@ for (k in 1:nk){
   en.temp.te2 <- exp(predict(en.fit2,  newx = ft_total_mat[te.ind,], type = "link", s = cv.fit.en2$lambda.min))
   en.pred.tr2 = log(en.temp.tr2)
   en.pred.te2 = log(en.temp.te2)
-  # en.pred.tr2 <- en.temp.tr2/(en.temp.tr2 + 1)
-  # en.pred.te2 <- en.temp.te2/(en.temp.te2 + 1)
+
   
   en.res2 <- survivalROC(Stime = surv.time[te.ind],
                          status  = status[te.ind],
